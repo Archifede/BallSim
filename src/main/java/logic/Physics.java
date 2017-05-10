6a99ab4d8dbe54242121611dd8542af30c8afdae
Archifede <bragipls@gmail.com>
2017-05-10 21:22:14 +0200

    fixed the swamp

--- src/main/java/logic/Physics.java
+++ src/main/java/logic/Physics.java
@@ -18,10 +18,11 @@ public class Physics {
 
     public static final double GRAVITATIONNAL_ACCELERATION = 9.81;
     public static final double BOUNCE_RATE = 0.66;
+    public static final double FLOOR_RATE = 0.9996;
 
 
     @SuppressWarnings("restriction") // This is for eclipse
-	public static void apply(Ball ball, double deltaT, int width, int height) {
+    public static void apply(Ball ball, double deltaT, int width, int height) {
 
         Point2D celerity = ball.getCelerity();
         Point2D coordinates = ball.getCoordinates();
@@ -29,26 +30,35 @@ public class Physics {
         double g = GRAVITATIONNAL_ACCELERATION;
         int d = (int) ball.getDiameter();
 
+        if (celerity.getY() >= -0.5 && celerity.getY() <= 0.5 && coordinates.getY() <= d / 2) {
+            coordinates = new Point2D(
+                    -tau * (celerity.getX()) * Math.exp(-deltaT / tau) + coordinates.getX() + tau * (celerity.getX()),
+                    d / 2);
+        } else
         coordinates = new Point2D(
                 -tau * (celerity.getX()) * Math.exp(-deltaT / tau) + coordinates.getX() + tau * (celerity.getX()),
                 -tau * (celerity.getY() + tau * g) * Math.exp(-deltaT / tau) - tau * g * deltaT + coordinates.getY() + tau * (celerity.getY() + tau * g));
-
-        if ((coordinates.getY() <= d/2 && celerity.getY() < 0 || coordinates.getY() >= height - d/2 && celerity.getY() > 0)
-                && (coordinates.getX() <= d/2 && celerity.getX() < 0 || coordinates.getX() >= width - d/2 && celerity.getX() > 0))
+        if (celerity.getY() >= -0.5 && celerity.getY() <= 0.5 && coordinates.getY() <= d / 2)
+            celerity = new Point2D(
+                    FLOOR_RATE * celerity.getX() * Math.exp(-deltaT / tau),
+                    0);
+        if ((coordinates.getY() <= d / 2 && celerity.getY() < 0 || coordinates.getY() >= height - d / 2 && celerity.getY() > 0)
+                && (coordinates.getX() <= d / 2 && celerity.getX() < 0 || coordinates.getX() >= width - d / 2 && celerity.getX() > 0))
             celerity = new Point2D(
                     (-BOUNCE_RATE) * (celerity.getX() * Math.exp(-deltaT / tau)),
                     (-BOUNCE_RATE) * ((celerity.getY() + tau * g) * Math.exp(-deltaT / tau) - tau * g));
 
-        else if (coordinates.getY() <= d/2 && celerity.getY() < 0
-                || coordinates.getY() >= height - d/2 && celerity.getY() > 0)
+        else if (coordinates.getY() <= d / 2 && celerity.getY() < 0
+                || coordinates.getY() >= height - d / 2 && celerity.getY() > 0)
             celerity = new Point2D(
                     celerity.getX() * Math.exp(-deltaT / tau),
                     (-BOUNCE_RATE) * (
                             (celerity.getY() + tau * g) * Math.exp(-deltaT / tau) - tau * g));
-        else if (coordinates.getX() <= d/2 && celerity.getX() < 0 || coordinates.getX() >= width - d/2 && celerity.getX() > 0)
+        else if (coordinates.getX() <= d / 2 && celerity.getX() < 0 || coordinates.getX() >= width - d / 2 && celerity.getX() > 0)
             celerity = new Point2D(
                     (-BOUNCE_RATE) * (celerity.getX() * Math.exp(-deltaT / tau)),
                     ((celerity.getY() + tau * g) * Math.exp(-deltaT / tau) - tau * g));
+
         else
             celerity = new Point2D(
                     celerity.getX() * Math.exp(-deltaT / tau),
